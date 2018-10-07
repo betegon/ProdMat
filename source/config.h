32e502101d95a0022c60d69f77b96b382a849251
betegon <miguelbetegongarcia@gmail.com>
2018-10-07 02:05:41 +0200

    quick test (giving values to matrices)

--- source/config.h
+++ source/config.h
@@ -1,11 +1,25 @@
+/*
+ * Fichero de configuracion de matrices
+ *
+ *
+ * Define el tamano de las matrices
+ * A[M][L] * B[L][N] = R[M][N]
+ */
+
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define M 4
-#define N 5
-#define L 6
+#define M 2
+#define N 2
+#define L 3
+/*
+ *tipo de datos
+ */
 typedef int prodMatT;
 
-void prodMat (prodMatT R[M][N],
+
+// Funcion a verificar
+void prodMat (
+		prodMatT R[M][N],
  		prodMatT A[M][L],
  		prodMatT B[L][N]);  // he declarado la función
--- source/prodMat.c
+++ source/prodMat.c
@@ -7,13 +7,55 @@
  */
 
 
-
 #include "config.h"
+#include <stdio.h>
+// Definimos la funcion. realizara: A[M][L] * B[L][N] = R[M][N]
 
-void prodMat (prodMatT R[M][N],
-		prodMatT A[M][L],
-		prodMatT B[L][N] ){  // he declarado la función
+const int A[2][3]=
+{
+    {1, 2, 3},
+    {2, 3, 4}
+};
 
+const int B[3][2]=
+{
+    {1, 2},
+    {2, 3},
+    {3, 4}
+};
+
+int R[2][2];
+
+
+void prodMat (
+		prodMatT R[M][N], // Matriz resultado
+		prodMatT A[M][L], // Primer operando
+		prodMatT B[L][N]){  // Segundo operando
+
+	int i,j,k, prod;
+	lazoFilas:	for(i=0;i<M;i++){ // ciclo por filas de A
+		LazoColumnas:	for(j=0;j<N;j++){ // ciclo por columnas de B
+			prod=0;
+			lazo: for(k=0;k<L;k++){ // ciclo por filas de B
+				prod+=A[i][k]*B[k][j];
+			}
+			R[i][j]=prod;
+		}
+	}
+	int row, columns;
+	for (int row=0; row<M; row++)
+	{
+	    for(int columns=0; columns<N; columns++)
+	        {
+	         printf("%d     ", R[row][columns]);
+	        }
+	    printf("\n");
+	 }
 }
 
 
+main (){
+	printf("Hellow world\n");
+	prodMat(R,A,B);
+
+}
